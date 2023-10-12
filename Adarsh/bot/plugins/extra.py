4b87b17d51eea7b74f290d8ff15f2de8106ead0d
Rishikesh Sharma(Rk_botz) <118276768+Rishikesh-Sharma09@users.noreply.github.com>
2023-10-12 14:19:23 +0530

    Update extra.py

--- Adarsh/bot/plugins/extra.py
+++ Adarsh/bot/plugins/extra.py
@@ -17,12 +17,12 @@ async def maintainers(b,m):
     except Exception:
                 await b.send_message(
                     chat_id=m.chat.id,
-                    text="I am Made By [GreyMatter's Bot](https://t.me/GreyMatter_Bots)",
+                    text="I am Made By [Rk_botz](https://t.me/Rk_botz)",
                     
                     reply_markup=InlineKeyboardMarkup(
                         [
                             [
-                                InlineKeyboardButton("Owner💻", url=f"https://t.me/GreyMatter_Owner")
+                                InlineKeyboardButton("Owner💻", url=f"https://t.me/Rk_botowner")
                             ]
                         ]
                     ),
@@ -42,7 +42,7 @@ async def follow_user(b,m):
                     reply_markup=InlineKeyboardMarkup(
                         [
                             [
-                                InlineKeyboardButton("Subscribe ❤️", url=f"https://youtube.com/@GreyMattersYT")
+                                InlineKeyboardButton("Subscribe ❤️", url=f"https://t.me/Rk_update")
                             ]
                         ]
                     ),
