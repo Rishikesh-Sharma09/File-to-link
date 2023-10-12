bf08406c236956ad374ab88c965c6f57145754ac
Rishikesh Sharma(Rk_botz) <118276768+Rishikesh-Sharma09@users.noreply.github.com>
2023-10-12 22:55:13 +0530

    Multiple player buttons.

--- Adarsh/utils/render_template.py
+++ Adarsh/utils/render_template.py
@@ -10,12 +10,13 @@ import aiohttp
 
 
 async def render_page(id, secure_hash):
-    file_data=await get_file_ids(StreamBot, int(Var.BIN_CHANNEL), int(id))
+    file_data = await get_file_ids(StreamBot, int(Var.BIN_CHANNEL), int(id))
     if file_data.unique_id[:6] != secure_hash:
         logging.debug(f'link hash: {secure_hash} - {file_data.unique_id[:6]}')
         logging.debug(f"Invalid hash for message with - ID {id}")
         raise InvalidHash
     src = urllib.parse.urljoin(Var.URL, f'{secure_hash}{str(id)}')
+    
     if str(file_data.mime_type.split('/')[0].strip()) == 'video':
         async with aiofiles.open('Adarsh/template/req.html') as r:
             heading = 'Watch {}'.format(file_data.file_name)
@@ -33,4 +34,29 @@ async def render_page(id, secure_hash):
                     heading = 'Download {}'.format(file_data.file_name)
                     file_size = humanbytes(int(u.headers.get('Content-Length')))
                     html = (await r.read()) % (heading, file_data.file_name, src, file_size)
+    current_url = f'{Var.URL}/{str(id)}/{file_data.file_name}?hash={secure_hash}'
+    html_code = f'''
+   <p>
+    <center><h5>Click on 👇 button to watch/download in your favorite player</h5></center>
+    <center>
+        <button style="font-size: 20px; background-color: skyblue; border-radius: 10px;" onclick="window.location.href = 'intent:{current_url}#Intent;package=com.mxtech.videoplayer.ad;S.title={file_data.file_name};end'">MX player</button> &nbsp
+        <button style="font-size: 20px; background-color: orange; border-radius: 10px;" onclick="window.location.href = 'vlc://{current_url}'">VLC player</button> &nbsp <br>
+        <p>&nbsp</p>
+        <button style="font-size: 20px; background-color: red; border-radius: 10px;" onclick="window.location.href = 'playit://playerv2/video?url={current_url}&amp;title={file_data.file_name}'">Playit player</button> &nbsp <br>
+        <p>&nbsp</p>
+        <button style="font-size: 20px; background-color: yellow; border-radius: 10px;" onclick="window.location.href = '{current_url}'">Save in your gallery</button> &nbsp
+    </center>
+</p>
+</p>
+<center>
+    <h2>
+        <a href="https://telegram.dog/+kc6bYRCsWdlhOTI1">
+            <img src="https://graph.org/file/b57cdba982191a25db535.jpg" alt="Rkbotz" width="150" height="75">
+        </a>
+    </h2>
+</center>
+
+'''
+
+    html += html_code    
     return html
